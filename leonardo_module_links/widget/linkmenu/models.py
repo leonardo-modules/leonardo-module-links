5f2d450a4c433197cb6f6248cd7bb52144505089
Michael Kutý <6du1ro.n@gmail.com>
2015-09-30 23:16:49 +0200

    use widget list benefits

--- leonardo_module_links/widget/linkmenu/models.py
+++ leonardo_module_links/widget/linkmenu/models.py
@@ -9,10 +9,15 @@ from leonardo_module_links.models import LinkCategory, Link
 
 
 class LinkMenuWidget(ListWidget):
-    list = models.ForeignKey(LinkCategory, verbose_name=_("link category"), related_name="%(app_label)s_%(class)s_related")
+    list = models.ForeignKey(LinkCategory, verbose_name=_(
+        "link category"), related_name="%(app_label)s_%(class)s_related")
 
     def get_links(self):
+        '''DEPRECATED use get_items'''
         return Link.objects.filter(category=self.list, visible=True).order_by('ordering')
+
+    def get_items(self, **kwargs):
+        return self.list.link_set.filter(visible=True).order_by('ordering')
 
     class Meta:
         abstract = True
